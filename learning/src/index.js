bbe66140092d6a6ed28c9d13425b5a48e552f792
Rahul Babbar <rahul.babbar@arya.ag>
2025-06-08 18:49:10 +0530

    create component and reuse inside another

--- learning/src/index.js
+++ learning/src/index.js
@@ -2,7 +2,20 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 
 function App() {
-  return <h1> First App2</h1>;
+  // function Toy() {
+  //   return <div>toy</div>;
+  // }
+  return (
+    <div>
+      <h1> First App2</h1>
+      <Toy />
+      <Toy />
+    </div>
+  );
+}
+
+function Toy() {
+  return <div>toy</div>;
 }
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
