cf9bd10321efe3a40fb50271f4c6b1b6a55388c0
Rahul Babbar <rahul.babbar@arya.ag>
2025-06-10 17:46:52 +0530

    you can use multiple return statements in components based on condition view can change;

--- learning/src/index.js
+++ learning/src/index.js
@@ -46,16 +46,17 @@ function Toy(props) {
 const toys = [];
 
 function Menu() {
+  if (toys.length === 0) {
+    return <h3>No Toys in store</h3>;
+  }
   return (
-    toys.length > 0 && (
-      <div className="menu">
-        {/* <Toy name="Duck" price="12" qty={12} />
+    <div className="menu">
+      {/* <Toy name="Duck" price="12" qty={12} />
       <Toy name="Spiderman" price="120" qty={15} /> */}
-        {toys.map((toy) => (
-          <Toy key={toy.name} toy={toy} />
-        ))}
-      </div>
-    )
+      {toys.map((toy) => (
+        <Toy key={toy.name} toy={toy} />
+      ))}
+    </div>
   );
 }
 
