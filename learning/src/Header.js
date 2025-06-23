c89975ffae90b5eb093479eaa340b11193be19b1
Rahul Babbar <rahul.babbar@arya.ag>
2025-06-24 05:28:11 +0530

    what is the requirement of useEffect;

--- learning/src/Header.js
+++ learning/src/Header.js
@@ -1,10 +1,10 @@
-import React from "react";
+import React, { useState } from "react";
 
 export const Header = () => {
   // inline-css
+  const [a, setA] = useState(new Date().toLocaleTimeString());
+  // setA(new Date().toLocaleTimeString());
   return (
-    <h1 style={{ color: "red", fontFamily: "arial" }}>
-      Toy Shop Opened {new Date().toLocaleTimeString()}
-    </h1>
+    <h1 style={{ color: "red", fontFamily: "arial" }}>Toy Shop Opened {a}</h1>
   );
 };
