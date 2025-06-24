54eb9a15fb5be3c84eab0c6f6d9dbb7bd3a00c57
Rahul Babbar <rahul.babbar@arya.ag>
2025-06-24 05:37:14 +0530

    useEffect first scenario;

--- learning/src/Header.js
+++ learning/src/Header.js
@@ -1,9 +1,14 @@
 import React, { useState } from "react";
+import { useEffect } from "react";
 
 export const Header = () => {
   // inline-css
   const [a, setA] = useState(new Date().toLocaleTimeString());
-  // setA(new Date().toLocaleTimeString());
+
+  useEffect(() => {
+    setA(new Date().toLocaleTimeString());
+  }, []);
+
   return (
     <h1 style={{ color: "red", fontFamily: "arial" }}>Toy Shop Opened {a}</h1>
   );
